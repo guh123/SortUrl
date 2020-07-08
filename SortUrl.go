7d75ae6f1f23957c59276f917eda7c29c8030f32
guh123 <1803350493@qq.com>
2020-07-08 18:12:21 +0800

    change silimir engine
    change silimir engine

--- SortUrl.go
+++ SortUrl.go
@@ -8,55 +8,14 @@ import (
 	"os"
 	"strings"
 	"sync"
+
+	"github.com/xrash/smetrics"
 )
 
 var urlcontent []string = make([]string, 0)
 var channel = make(chan string)
 var wg sync.WaitGroup
 
-func SimilarText(first, second string, percent *float64) int {
-	var similarText func(string, string, int, int) int
-	similarText = func(str1, str2 string, len1, len2 int) int {
-		var sum, max int
-		pos1, pos2 := 0, 0
-
-		// Find the longest segment of the same section in two strings
-		for i := 0; i < len1; i++ {
-			for j := 0; j < len2; j++ {
-				for l := 0; (i+l < len1) && (j+l < len2) && (str1[i+l] == str2[j+l]); l++ {
-					if l+1 > max {
-						max = l + 1
-						pos1 = i
-						pos2 = j
-					}
-				}
-			}
-		}
-
-		if sum = max; sum > 0 {
-			if pos1 > 0 && pos2 > 0 {
-				sum += similarText(str1, str2, pos1, pos2)
-			}
-			if (pos1+max < len1) && (pos2+max < len2) {
-				s1 := []byte(str1)
-				s2 := []byte(str2)
-				sum += similarText(string(s1[pos1+max:]), string(s2[pos2+max:]), len1-pos1-max, len2-pos2-max)
-			}
-		}
-
-		return sum
-	}
-
-	l1, l2 := len(first), len(second)
-	if l1+l2 == 0 {
-		return 0
-	}
-	sim := similarText(first, second, l1, l2)
-
-	*percent = float64(sim*200) / float64(l1+l2)
-
-	return sim
-}
 func main() {
 
 	for i := 1; i <= 30; i++ {
@@ -68,7 +27,11 @@ func main() {
 	for sc.Scan() {
 		url := sc.Text()
 		if strings.Index(url, "http") == -1 {
-			url = "http://" + url
+			http_url := "http://" + url
+			https_url := "https://" + url
+
+			channel <- http_url
+			channel <- https_url
 		}
 		channel <- url
 
@@ -93,11 +56,10 @@ func deal_url() {
 		}
 
 		for _, value := range urlcontent {
-			var percent *float64 = new(float64)
-			SimilarText(string(resp), value, percent)
+			var percent float64 = smetrics.JaroWinkler(string(resp), value, 0.7, 4)
 
-			fmt.Println(*percent)
-			if *percent > 30 {
+			fmt.Println(percent)
+			if percent > 0.95 {
 
 				count = 1
 				break
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/guh123/SortUrl
 
 go 1.14
+
+require (
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/masatana/go-textdistance v0.0.0-20191005053614-738b0edac985 // indirect
+	github.com/xrash/smetrics v0.0.0-20170218160415-a3153f7040e9 // indirect
+)
